bd44d9f1b81115c7922c63275fadd2a14ccfa44c
morganjb52 <93269477+morganjb52@users.noreply.github.com>
2025-05-01 23:51:59 -0400

    Updates from CGPT first pass

---
+++ src/AlbumGrid.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './AlbumGrid.css';
+
+interface Album {
+  id: number;
+  basic_information: {
+    title: string;
+    cover_image: string;
+    artists: { name: string }[];
+  };
+}
+
+const AlbumGrid: React.FC = () => {
+  const [albums, setAlbums] = useState<Album[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.discogs.com/users/YOUR_USERNAME/collection/folders/0/releases', {
+        headers: {
+          Authorization: 'Discogs token=YOUR_TOKEN',
+          'User-Agent': 'MyDiscogsApp/1.0',
+        },
+      })
+      .then((res) => setAlbums(res.data.releases))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="grid">
+      {albums.map((item) => (
+        <div key={item.id} className="album-card">
+          <img
+            src={item.basic_information.cover_image}
+            alt={item.basic_information.title}
+          />
+          <h4>{item.basic_information.title}</h4>
+          <p>{item.basic_information.artists[0]?.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AlbumGrid;
