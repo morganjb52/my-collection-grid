d63097e735c35261c5c525e6f3b85be28c0338ef
morganjb52 <93269477+morganjb52@users.noreply.github.com>
2025-05-01 23:40:40 -0400

    app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AlbumGrid from './components/AlbumGrid';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>My Discogs Collection</h1>
+      <AlbumGrid />
+    </div>
+  );
+}
+
+export default App;
